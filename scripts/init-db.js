5084eef4165b04a4f1666ce369f46785fb6d2317
[dyad] <randy@cvlcvc.com>
2025-11-12 16:54:05 -0500

    [dyad] Fixing server crash by ensuring environment variables are loaded - wrote 3 file(s)

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -1,6 +1,8 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
+
 const { Client } = require('pg');
 const fs = require('fs');
-const path = require('path');
 
 async function initializeDatabase() {
   const connectionString = process.env.DATABASE_URL;
